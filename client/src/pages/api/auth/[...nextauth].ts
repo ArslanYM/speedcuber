a49b4e43faf3aac475b10ca11f936546349653a0
ArslanYM <arsalanyaqmalik@gmail.com>
2024-02-21 14:57:45 +0530

    bugfix: fixed build issue

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -5,8 +5,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.NEXTAUTH_GITHUB_CLIENT_ID,
-      clientSecret: process.env.NEXTAUTH_GITHUB_CLIENT_SECRET,
+      clientId: process.env.NEXTAUTH_GITHUB_CLIENT_ID?? '',
+      clientSecret: process.env.NEXTAUTH_GITHUB_CLIENT_SECRET?? '',
     }),
     // ...add more providers here
   ],
