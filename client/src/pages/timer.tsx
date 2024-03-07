fd5fefb2188739f69a7069ad8f5f53226410a561
ArslanYM <arsalanyaqmalik@gmail.com>
2024-03-07 13:01:39 +0530

    fixed timer

--- client/src/pages/timer.tsx
+++ client/src/pages/timer.tsx
@@ -1,14 +1,17 @@
 import { useState, useEffect, useRef } from "react";
 import generateShuffle from "../../scrambleGenerator";
+import { Button } from "@/components/ui/button";
 
-//TODO : save timer time for current solve
 const Timer: React.FC = () => {
   const [scramble, setScramble] = useState<string>("");
   const [time, setTime] = useState<number>(0);
   const [running, setRunning] = useState<boolean>(false);
-  const [lastTime, setLastTime] = useState<number>(0);
-
+  const [showResult, setShowResult] = useState<boolean>(false);
   let interval: NodeJS.Timeout;
+
+  useEffect(() => {
+    setScramble(generateShuffle());
+  }, []);
 
   useEffect(() => {
     if (running) {
@@ -19,22 +22,18 @@ const Timer: React.FC = () => {
     if (!running) {
       clearInterval(interval);
     }
-
     return () => clearInterval(interval);
   }, [running]);
 
   useEffect(() => {
-    setScramble(generateShuffle());
-    const handleKeyDown = (event: KeyboardEvent) => {
+    const handleKeyDown =  (event: KeyboardEvent) => {
       if (event.code === "Space") {
         if (running) {
-          setLastTime(time);
           setRunning(false);
-          setTime(0);
-          setScramble(generateShuffle());
+          setShowResult(true);
         } else {
           setRunning(true);
-          setScramble(generateShuffle());
+          setShowResult(false);
         }
       }
     };
@@ -44,44 +43,54 @@ const Timer: React.FC = () => {
     };
   }, [running]);
 
+  const handleReset = () => {
+    setTime(0);
+    setRunning(false);
+    setShowResult(false);
+    setScramble(generateShuffle());
+  };
+
   return (
     <section className="text-gray-600 font-mono">
-      <div className="container mx-auto flex  items-center justify-center flex-col">
+      <div className="container mx-auto flex items-center justify-center flex-col">
         <div className="text-center lg:w-2/3 w-full">
-          {running ? (
-            <>
-              <div className="pt-32 pb-32">
-                <p
-                  className="  text-black  font-extrabold cursor-pointer"
-                  onClick={() => {
-                    setRunning(false);
-                  }}
-                >
-                  <span className="text-9xl">
-                    {Math.floor((time / 1000) % 60)}
-                  </span>
-                  <span className="text-5xl">
-                    :{("0" + ((time / 10) % 100)).slice(-2)}
-                  </span>
-                </p>
-              </div>
-            </>
+          {showResult ? (
+            <div className="m-36  font-extrabold">
+              <h1 className="text-black">
+                <span className="text-9xl">
+                  {Math.floor((time / 1000) % 60)}
+                </span>
+                <span className="text-5xl">
+                  .{("0" + ((time / 10) % 100)).slice(-2)}
+                </span>
+                <span className="text-4xl">s</span>
+              </h1>
+              <Button variant={"ghost"} onClick={handleReset} className="mt-4 ">
+                Reset
+              </Button>
+            </div>
+          ) : running ? (
+            <div className="pt-32 pb-32">
+              <p className="text-black font-extrabold">
+                <span className="text-9xl">
+                  {Math.floor((time / 1000) % 60)}
+                </span>
+                <span className="text-5xl">
+                  :{("0" + ((time / 10) % 100)).slice(-2)}
+                </span>
+              </p>
+            </div>
           ) : (
             <div className="m-2">
               <h1 className="text-4xl font-semibold font-mono text-black">
                 {scramble}
               </h1>
               <div className="p-12 m-6">
-                <p className="  text-black  font-extrabold">
-                  <span className="text-9xl">
-                    {Math.floor((time / 1000) % 60)}
-                  </span>
-                  <span className="text-5xl">
-                    :{("0" + ((time / 10) % 100)).slice(-2)}
-                  </span>
+                <p className="text-black font-extrabold">
+                  <span className="text-9xl">0</span>
+                  <span className="text-5xl">:00</span>
                 </p>
               </div>
-
               <h1
                 onClick={() => {
                   setRunning(true);
